f76383c2dd833707f7a88362e4504f3879819292
Squ1reX <maksimhripinkov658@gmail.com>
2023-08-19 20:47:03 +0300

    update

--- main2.py
+++ main2.py
@@ -1,7 +1,21 @@
 import sqlite3
+import random
+import string
 from tkinter import *
 from tkinter import PhotoImage
 import tkinter as tk
+
+def generate_token(length=28):
+    characters = string.ascii_letters + string.digits
+    token = ''.join(random.choice(characters) for _ in range(length))
+    return token
+
+conn = sqlite3.connect('users.db')
+cursor = conn.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT, token TEXT)''')
+conn.commit()
+conn.close()
+
 
 def register():
     username = register_username_entry.get()
@@ -16,10 +30,13 @@ def register():
     if existing_user:
         register_status.config(text="Пользователь с таким именем уже существует.")
     else:
-        cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+        token = generate_token()
+        cursor.execute("INSERT INTO users (username, password, token) VALUES (?, ?, ?)", (username, password, token))
         conn.commit()
         conn.close()
         register_status.config(text="Регистрация успешна!")
+
+
 
 def login():
     username = login_username_entry.get()
@@ -32,10 +49,16 @@ def login():
     conn.close()
 
     if user:
+        print(user)  # Вывод структуры кортежа для определения правильного индекса
         login_status.config(text="Вход выполнен успешно.")
-        open_token_window()
+        if len(user) >= 3:
+            open_token_window(user[2])  # Проверка, что индекс существует
+        else:
+            print("Неправильная структура кортежа")
     else:
         login_status.config(text="Неверное имя пользователя или пароль.")
+
+
 
 def open_login_register_window():
     global register_username_entry, register_password_entry, login_username_entry, login_password_entry, register_status, login_status, login_register_window
@@ -95,6 +118,7 @@ def open_messenger_window():
     messenger_window.geometry("500x550+{}+{}".format(int(main_window.winfo_screenwidth()/2 - 250), int(main_window.winfo_screenheight()/2 - 275)))
     logo = tk.PhotoImage(file='pngimage.png')
     messenger_window.iconphoto(False, logo)
+    messenger_window.resizable(False, False)
 
     message_box = Text(messenger_window)
     message_box.pack()
@@ -106,24 +130,15 @@ def open_messenger_window():
     send_button = Button(messenger_window, text="Send", command=send_message)
     send_button.pack()
 
-conn = sqlite3.connect('users.db')
-cursor = conn.cursor()
-cursor.execute('''CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT)''')
-conn.commit()
-conn.close()
+# Остальной код остается таким же, как в предыдущем сообщении.
 
-conn = sqlite3.connect('messenger.db')
-cursor = conn.cursor()
-cursor.execute('CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY AUTOINCREMENT, sender TEXT, receiver TEXT, message TEXT)')
-conn.commit()
-conn.close()
 
 def open_chat_window(token):
     global chat_window
     chat_window = Toplevel(main_window)
     chat_window.title("Чат")
     chat_window.geometry("300x150+{}+{}".format(int(main_window.winfo_screenwidth()/2 - 150), int(main_window.winfo_screenheight()/2 - 75)))
-
+    chat_window.resizable(False, False)
     token_label = Label(chat_window, text="Токен пользователя: " + token)
     token_label.pack()
 
@@ -137,12 +152,12 @@ def open_chat_window(token):
     send_button = Button(chat_window, text="Send", command=send_message)
     send_button.pack()
 
-def open_token_window():
+def open_token_window(token):
     global token_window
     token_window = Toplevel(main_window)
     token_window.title("Введите токен")
     token_window.geometry("300x150+{}+{}".format(int(main_window.winfo_screenwidth()/2 - 150), int(main_window.winfo_screenheight()/2 - 75)))
-
+    token_window.resizable(False, False)
     token_label = Label(token_window, text="Введите токен пользователя:")
     token_label.pack()
 
@@ -152,18 +167,29 @@ def open_token_window():
     open_chat_button = Button(token_window, text="Открыть чат", command=lambda: open_chat_window(token_entry.get()))
     open_chat_button.pack()
 
-main_window = Tk()
-main_window.title("Main Window")
+conn = sqlite3.connect('users.db')
+cursor = conn.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT, token TEXT)''')
+conn.commit()
+conn.close()
 
-login_register_button = Button(main_window, text="Вход и регистрация", command=open_login_register_window)
-login_register_button.pack()
+def open_main_window():
+    global main_window
+    main_window = Tk()
+    main_window.title("Enter Window")
 
-main_window.geometry("300x200+{}+{}".format(int(main_window.winfo_screenwidth()/2 - 150), int(main_window.winfo_screenheight()/2 - 100)))
-main_window.resizable(False, False)
-logo = tk.PhotoImage(file='pngimage.png')
-main_window.iconphoto(False, logo)
+    login_register_button = Button(main_window, text="Вход и регистрация", command=open_login_register_window)
+    login_register_button.pack()
 
-main_window.mainloop()
+    main_window.geometry("300x200+{}+{}".format(int(main_window.winfo_screenwidth()/2 - 150), int(main_window.winfo_screenheight()/2 - 100)))
+    main_window.resizable(False, False)
+    logo = tk.PhotoImage(file='pngimage.png')
+    main_window.iconphoto(False, logo)
+    main_window.resizable(False, False)
+
+    main_window.mainloop()
+
+open_main_window()
 
 
 
